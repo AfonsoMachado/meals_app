eb8c2bae6a825a394ef96b3bb63f7d177bb0ce45
Afonso Machado <afonsosmachado@gmail.com>
2023-09-28 18:00:16 -0300

    feat: show meal on his category

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -9,12 +10,20 @@ class CategoriesMealsScreen extends StatelessWidget {
     // Pegando a categoria passada para a rota por argumentos
     final category = ModalRoute.of(context)?.settings.arguments as Category;
 
+    // Pegando as refeições por categoria
+    final categoryMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
